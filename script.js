4525a25767280873f686d96a136474e606591bb2
Kendra Moon <kendra.moon@gmail.com>
2022-07-07 15:22:37 -0500

    add dimensions to grid div

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let gridSize = 4
+let gridSize = 16
 const gridContainer = document.getElementById('grid-container');
 
-for (i=1; i <= gridSize; i++) {
+for (i=1; i <= gridSize ** 2; i++) {
     const div = document.createElement('div');
     gridContainer.append(div);
     div.setAttribute('class', 'grid-item');
