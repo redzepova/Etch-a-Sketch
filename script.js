59b08e3cfaca8e2bb522c4b5289e548125c28bcf
Kendra Moon <kendra.moon@gmail.com>
2022-07-08 14:55:25 -0500

    add variable grid size

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let gridSize = 16
+let gridSize = 16;
 const gridContainer = document.getElementById('grid-container');
+
+gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
 for (i=1; i <= gridSize ** 2; i++) {
     const div = document.createElement('div');
     gridContainer.append(div);
     div.setAttribute('class', 'grid-item');
 };
+ 
+let gridItem = document.querySelectorAll(".grid-item");
+
+console.log(gridItem.length);
+
+gridItem.forEach(element => {
+    console.log(gridItem.length);
+});
+
