6ba605c98b3d4430f6ba1650cbb8b5fff8453e83
Kendra Moon <kendra.moon@gmail.com>
2022-07-09 09:48:31 -0500

    fix grid container size

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+
+//A grid exists. There is a grid. Hello, grid!
+//At some point, I would like this to be user input.
 let gridSize = 16;
 const gridContainer = document.getElementById('grid-container');
 
 gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
+//Depending on the grid size, create the elements that populate the grid.
+//There should be gridsize squared number of elements
+//i want each iteration of this to be a square.
 for (i=1; i <= gridSize ** 2; i++) {
     const div = document.createElement('div');
     gridContainer.append(div);
@@ -15,5 +21,6 @@ console.log(gridItem.length);
 
 gridItem.forEach(element => {
     console.log(gridItem.length);
+    element.addEventListener("hover")
 });
 
