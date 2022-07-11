d5725a4b72f068ed26a9b44281858e6d0de74041
Kendra Moon <kendra.moon@gmail.com>
2022-07-11 13:33:36 -0500

    add functionality to color picker

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //set up grid//
 let gridSize = 40;
 const gridContainer = document.getElementById('grid-container');
+let userColor = "red"
 
 gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
@@ -9,6 +10,7 @@ for (i=1; i <= gridSize ** 2; i++) {
     const div = document.createElement('div');
     gridContainer.append(div);
     div.setAttribute('class', 'grid-item');
+    console.log(userColor);
 };
 
 //loop through node list and add mousedown event listener to each child of grid.
@@ -20,5 +22,6 @@ gridItem.forEach(element => {
 });
 
 function changeColor (e) {
-    e.target.style.backgroundColor = "red";
+    userColor = document.getElementById('color').value;
+    e.target.style.backgroundColor = userColor;
 };
