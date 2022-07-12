3035607a435bf2f76de9f91b995581d1439c5f8b
Kendra Moon <kendra.moon@gmail.com>
2022-07-12 10:34:44 -0500

    Fix slider and grid create function

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 //set up grid//
-let gridSize = 40;
+
+
+const sizeSelector = document.getElementById('grid-size');
+let gridSize = 40
 const gridContainer = document.getElementById('grid-container');
 let userColor = document.getElementById('color').value;
+const gridItem = document.querySelectorAll('.grid-item');
+
 
 gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
+sizeSelector.addEventListener("input", () => {
+    removeGrid(gridContainer);
+    createGrid();
+});
+
 //grid pixels are added with class grid-item.
-for (i=1; i <= gridSize ** 2; i++) {
-    const div = document.createElement('div');
-    gridContainer.append(div);
-    div.setAttribute('class', 'grid-item');
-    console.log(userColor);
+
+function createGrid () {
+    gridSize = sizeSelector.value;
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    for (i=1; i <= gridSize ** 2; i++) {
+        const div = document.createElement('div');
+        gridContainer.append(div);
+        div.setAttribute('class', 'grid-item');
+        console.log(userColor);
+    };
+};
+
+function removeGrid (parent) {
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    };
 };
 
 //loop through node list and add mousedown event listener to each child of grid.
-const gridItem = document.querySelectorAll('.grid-item');
+
 
 gridItem.forEach(element => {
     element.addEventListener('mouseover', changeColor);
